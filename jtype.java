c36c199870625e0a48edb2c08a96220fba86d887
Javier <illeskasjavi333@gmail.com>
2022-04-29 11:01:16 +0200

    El primer commit

---
+++ jtype.java
@@ -0,0 +1,20 @@
+package es.maestredam.tema07;
+
+/**
+ * 
+ * Aplicación que bla bla
+ * @version 1.1
+ * @author illes
+ * @date 29 abr 2022
+ */
+public class jtype {
+
+    /**
+     * Main method
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printf("Ejecutando%s%n", "jtype");
+    }
+
+}
